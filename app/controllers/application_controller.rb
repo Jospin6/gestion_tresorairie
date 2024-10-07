100b85b1d9a300ca87bff72e3ad26a931b5332e0
jospin6 <jospinndagano1@gmail.com>
2024-10-07 13:59:51 +0200

    Config user form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_devise_parameters, if: :devise_controller?
+    def configure_devise_parameters
+        devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username, :role, :email, :password, :password_confirmation, :isActive)}
+    end
 end
