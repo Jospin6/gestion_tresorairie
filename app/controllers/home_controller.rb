f131d5181216a59449c5f1a7acf3b747d13b3e51
jospin6 <jospinndagano1@gmail.com>
2024-10-17 19:53:27 +0200

    Display data on the dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
     def index
+
+        @projets_finances = Projet.where(statut: "financé").count
+        @projets_non_finances = Projet.where(statut: "non financé").count
+        @partenaires = Partenaire.count
+        @montant_apport_usd = HistoriqueApport.where(devise: "USD").sum(:montant_recu)
+        @montant_apport_fc = HistoriqueApport.where(devise: "FC").sum(:montant_recu)
+        @depenses_usd = AutresDepense.where(devise: "USD").sum(:montant)
+        @depenses_fc = AutresDepense.where(devise: "FC").sum(:montant)
+
+        @projets = Projet.all
+        @depenses = AutresDepense.all
         
     end
 
